79f6a8590cb58378963f1986bfd5581da7e71a83
Thomas Tighe <ttighe@reisystems.com>
2019-04-03 11:11:24 -0400

    generic path for copy

--- setup.js
+++ setup.js
@@ -3,11 +3,11 @@
 const copy = require('copy');
 
 
-copy('./node_modules/uswds/dist/fonts/**/*.*', './src/fonts', function(err, files) {
+copy('**/node_modules/uswds/dist/fonts/**/*.*', './src/fonts', function(err, files) {
   if (err) throw err;
   // `files` is an array of the files that were copied
 });
-copy('./node_modules/uswds/dist/img/**/*.*', './src/img', function(err, files) {
+copy('**/node_modules/uswds/dist/img/**/*.*', './src/img', function(err, files) {
   if (err) throw err;
   // `files` is an array of the files that were copied
 });
