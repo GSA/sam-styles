a0e47621cdecdb34d2204e7be66f060607cff3e0
samanthabaker <samantha.lynneb@gmail.com>
2019-04-23 13:13:33 -0400

    making icons into svg format

--- src/js/components/icons.js
+++ src/js/components/icons.js
@@ -8,6 +8,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { samBars } from "./../icons/bars";
 import { samArrowLeft } from "./../icons/arrowLeft";
+import { samAlarm } from "./../icons/alarm";
+import { samCalendar } from "./../icons/calendar";
+import { samDelete } from "./../icons/delete";
+import { samFilter } from "./../icons/filter";
+import { samGlossary } from "./../icons/glossary";
+import { samHome } from "./../icons/home";
+import { samMessage } from "./../icons/message";
+import { samNewMessage } from "./../icons/newMessage";
+import { samPrinter } from "./../icons/printer";
+import { samReportData } from "./../icons/reportData";
+import { samShare } from "./../icons/share";
+import { samTools } from "./../icons/tools";
+import { samWhatsNext } from "./../icons/whatsNext";
+import { samWorkspace } from "./../icons/workspace";
 
 config.familyPrefix = "icon";
 
@@ -18,7 +32,21 @@ const icons = [
   faTh,
   faSignOutAlt,
   samBars,
-  samArrowLeft
+  samArrowLeft,
+  samAlarm,
+  samCalendar,
+  samDelete,
+  samFilter,
+  samGlossary,
+  samHome,
+  samMessage,
+  samNewMessage,
+  samPrinter,
+  samReportData,
+  samShare,
+  samTools,
+  samWhatsNext,
+  samWorkspace
 ];
 
 library.add(...icons);
