6b89d60eac658191c8a91ad22ecd8199e0d5dceb
tigerligsa <congyuan.li@gsa.gov>
2019-08-16 14:48:38 -0400

    new icons

---
+++ src/icons/sdsRequest.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'sds';
+var iconName = 'request';
+var width = 512;
+var height = 512;
+var ligatures = [];
+var unicode = '';
+var svgPathData = 'M512,155.29v345.1A11.54,11.54,0,0,1,500.54,512H11.46A11.54,11.54,0,0,1,0,500.38V11.61A11.54,11.54,0,0,1,11.46,0H348.74A11.54,11.54,0,0,1,360.2,11.61a11.54,11.54,0,0,1-11.46,11.61H22.93V488.77H489.07V155.29a11.46,11.46,0,1,1,22.93,0ZM151.95,289.82l222.83-225a11.35,11.35,0,0,1,16.25,0L448.21,123a11.86,11.86,0,0,1,0,16.46l-222.83,225A11.43,11.43,0,0,1,220,367.6S142.27,387,141.36,387c-7.27,0-12.88-7.3-11.16-14.42l18.7-77.25A11.66,11.66,0,0,1,151.95,289.82ZM157,359.38l54.36-13.61L423.82,131.22l-41-41.72L170.46,304ZM508.64,78,473.44,113.8a11.38,11.38,0,0,1-16.28,0L400,55.57a11.74,11.74,0,0,1,0-16.43L435.17,3.3a11.69,11.69,0,0,1,16.28,0l57.18,58.23A11.74,11.74,0,0,1,508.64,78Zm-24.34-8.22L443.32,28l-19,19.33,41,41.72Z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.sdsRequest = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
