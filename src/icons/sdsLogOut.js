f859a3566a440452d673e8370de11dd2411de92f
tigerligsa <congyuan.li@gsa.gov>
2019-08-20 16:40:58 -0400

    new secondary icons

---
+++ src/icons/sdsLogOut.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'sds';
+var iconName = 'log-out';
+var width = 512;
+var height = 512;
+var ligatures = [];
+var unicode = '';
+var svgPathData = 'M500.5,0H11.5C5.1,0,0,5.1,0,11.5v489.1c0,6.3,5.1,11.5,11.5,11.5h489.1c6.3,0,11.5-5.1,11.5-11.5v-87.4c0-6.3-5.1-11.5-11.5-11.5s-11.5,5.1-11.5,11.5v75.9H22.9V22.9h466.1v76c0,6.3,5.1,11.5,11.5,11.5s11.5-5.1,11.5-11.5V11.5C512,5.1,506.9,0,500.5,0z M362.1,416.2c5,4.3,12.6,3.8,16.9-1.2c0,0,0,0,0,0l130.1-151.1c3.9-4.5,3.9-11.2,0-15.7L379,97.1c-4.3-5-11.9-5.6-16.9-1.3c-5,4.3-5.6,11.9-1.3,16.9l113,131.3H100c-6.6,0-12,5.4-12,12s5.4,12,12,12h373.8l-113,131.3C356.5,404.3,357.1,411.9,362.1,416.2z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.sdsLogOut = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
