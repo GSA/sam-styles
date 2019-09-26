0bbb578d8a9f11e2bc7d137a35ad9f7294f54b1a
samanthabaker <samantha.lynneb@gmail.com>
2019-09-26 13:13:16 -0400

    icons added

--- src/icons/sdsExclamationCircle.js
+++ src/icons/sdsExclamationCircle.js
@@ -1,12 +1,12 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 var prefix = 'sds';
-var iconName = 'user';
+var iconName = 'exclamation-circle';
 var width = 512;
 var height = 512;
 var ligatures = [];
 var unicode = '';
-var svgPathData = 'M500.54,512a11.47,11.47,0,0,1-11.46-11.46c0-104.07-68.64-185-179.13-211.17a11.46,11.46,0,0,1-2-21.63,127.94,127.94,0,1,0-103.85,0,11.46,11.46,0,0,1-2,21.63C91.57,315.54,22.93,396.45,22.93,500.52a11.46,11.46,0,0,1-22.93,0C0,395.75,65.81,309.6,169.94,274.69a151,151,0,0,1-64.81-123.82C105.13,67.68,172.81,0,256,0S406.87,67.68,406.87,150.88a151,151,0,0,1-64.81,123.82C446.19,309.6,512,395.75,512,500.52A11.47,11.47,0,0,1,500.54,512Z'
+var svgPathData = 'M256,0C114.61,0,0,114.61,0,256S114.61,512,256,512,512,397.39,512,256,397.39,0,256,0Zm0,450.19a36.52,36.52,0,1,1,36.52-36.52A36.52,36.52,0,0,1,256,450.19Zm47.26-315.89L269.45,337.19H244L210.21,134.31V66.68h93.05Z'
 exports.definition = {
   prefix: prefix,
   iconName: iconName,
@@ -18,7 +18,7 @@ exports.definition = {
     svgPathData
   ]};
 
-exports.sdsUser = exports.definition;
+exports.sdsExclamationCircle = exports.definition;
 exports.prefix = prefix;
 exports.iconName = iconName;
 exports.width = width;
--- src/icons/sdsFile.js
+++ src/icons/sdsFile.js
@@ -1,12 +1,12 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 var prefix = 'sds';
-var iconName = 'user';
+var iconName = 'file';
 var width = 512;
 var height = 512;
 var ligatures = [];
 var unicode = '';
-var svgPathData = 'M500.54,512a11.47,11.47,0,0,1-11.46-11.46c0-104.07-68.64-185-179.13-211.17a11.46,11.46,0,0,1-2-21.63,127.94,127.94,0,1,0-103.85,0,11.46,11.46,0,0,1-2,21.63C91.57,315.54,22.93,396.45,22.93,500.52a11.46,11.46,0,0,1-22.93,0C0,395.75,65.81,309.6,169.94,274.69a151,151,0,0,1-64.81-123.82C105.13,67.68,172.81,0,256,0S406.87,67.68,406.87,150.88a151,151,0,0,1-64.81,123.82C446.19,309.6,512,395.75,512,500.52A11.47,11.47,0,0,1,500.54,512Z'
+var svgPathData = 'M22.92,489.08H356.1V367.57a11.47,11.47,0,0,1,11.46-11.46H489.08V22.93H22.92ZM169,187.13H343a11.46,11.46,0,0,1,0,22.93H169a11.46,11.46,0,0,1,0-22.93Zm0,53.55H343a11.46,11.46,0,0,1,0,22.92H169a11.46,11.46,0,0,1,0-22.92Zm0,53.55h116.7a11.46,11.46,0,0,1,0,22.93H169a11.46,11.46,0,0,1,0-22.93Z'
 exports.definition = {
   prefix: prefix,
   iconName: iconName,
@@ -18,7 +18,7 @@ exports.definition = {
     svgPathData
   ]};
 
-exports.sdsUser = exports.definition;
+exports.sdsFile = exports.definition;
 exports.prefix = prefix;
 exports.iconName = iconName;
 exports.width = width;
