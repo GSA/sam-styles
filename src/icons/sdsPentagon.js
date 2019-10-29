441f450d9e8b26eb7fa01ee913535fc932984017
Samantha Baker <38532587+samanthabaker@users.noreply.github.com>
2019-10-29 13:46:59 -0400

    new icons added

---
+++ src/icons/sdsPentagon.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'sds';
+var iconName = 'pentagon';
+var width = 543.5;
+var height = 512;
+var ligatures = [];
+var unicode = '';
+var svgPathData = 'M0,157.2l83.2,254.5h269.3l83.2-254.5L217.8,0L0,157.2z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.sdsPentagon = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
