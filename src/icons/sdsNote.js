d65d4c1bc8248ffecfa3773dd804a19c74384771
Alejandro Colina <jose.colina@gsa.gov>
2020-03-06 15:58:37 -0500

    Added sdsArticle and sdsNote icons

---
+++ src/icons/sdsNote.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'sds';
+var iconName = 'note';
+var width = 32;
+var height = 32;
+var ligatures = [];
+var unicode = '';
+var svgPathData = 'M23.7,29.6v-5.9h5.9L23.7,29.6z M1.4,1.4h29.1v20.8H23c-0.4,0-0.7,0.3-0.7,0.7v7.6H1.4V1.4z M32,23.2c0-0.1,0-0.1,0-0.2c0,0,0,0,0,0V0.7C32,0.3,31.7,0,31.3,0H0.7C0.3,0,0,0.3,0,0.7v30.6C0,31.7,0.3,32,0.7,32H23c0.1,0,0.2,0,0.3-0.1c0.1,0,0.2-0.1,0.2-0.1c0,0,0,0,0,0l8.3-8.3l0,0C31.9,23.4,31.9,23.3,32,23.2C31.9,23.3,31.9,23.2,32,23.2L32,23.2z M10.6,13.1h10.9c0.4,0,0.7-0.3,0.7-0.7c0-0.4-0.3-0.7-0.7-0.7H10.6c-0.4,0-0.7,0.3-0.7,0.7C9.8,12.8,10.2,13.1,10.6,13.1 M21.4,15H10.6c-0.4,0-0.7,0.3-0.7,0.7c0,0.4,0.3,0.7,0.7,0.7h10.9c0.4,0,0.7-0.3,0.7-0.7C22.2,15.4,21.8,15,21.4,15 M17.9,18.4h-7.3c-0.4,0-0.7,0.3-0.7,0.7c0,0.4,0.3,0.7,0.7,0.7h7.3c0.4,0,0.7-0.3,0.7-0.7C18.6,18.7,18.3,18.4,17.9,18.4';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.sdsNote = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
