dfc8d33a41dd3535d710464faccc5a7a2811fbd0
Alejandro Colina <jose.colina@gsa.gov>
2020-06-11 09:16:05 -0400

    Changes

--- src/components/03-components/03-buttons/buttons.config.js
+++ src/components/03-components/03-buttons/buttons.config.js
@@ -15,30 +15,102 @@ function variants() {
   for (let i = 0; i < len; i++) {
     let vname = variantNames[i]
     let vmods  = vname.split("-");
+    let title = _.capitalize(vname);
+
     let variant = {
       name: vname,
       context: {
         button: {
-          text: _.capitalize(vname),
+          text: title,
           modifiers: vmods,
         },
         hoverButton: {
-          text: _.capitalize(vname),
+          text: title,
           modifiers: [...vmods,"hover"],
         },
         activeButton: {
-          text: _.capitalize(vname),
+          text: title,
           modifiers: [...vmods,"active"],
         },
         focusButton: {
-          text: _.capitalize(vname),
+          text: title,
           modifiers: vmods,
           classes: "usa-focus",
         },
         disabledButton: {
-          text: _.capitalize(vname),
+          text: title,
           modifiers: vmods,
           disabled: true,
+        },
+        button_c: {
+          text: null,
+          icons: true,
+          modifiers: vmods,
+          smodifiers: ['circle'],
+        },
+        hoverButton_c: {
+          text: null,
+          icons: true,
+          modifiers: [...vmods,"hover"],
+          smodifiers: ['circle'],
+        },
+        activeButton_c: {
+          text: null,
+          icons: true,
+          modifiers: [...vmods,"active"],
+          smodifiers: ['circle'],
+        },
+        focusButton_c: {
+          text: null,
+          modifiers: null,
+          icons: true,
+          classes: "usa-focus",
+          smodifiers: ['circle'],
+        },
+        disabledButton_c: {
+          text: null,
+          icons: true,
+          modifiers: vmods,
+          smodifiers: ['circle'],
+          disabled: true,
+        },
+
+
+
+
+
+        
+        bigButton: {
+          text: title,
+          modifiers: [...vmods,"big"],
+        },
+        bigButton_c: {
+          text: null,
+          icons: true,
+          modifiers: vmods,
+          smodifiers: ['circle','big'],
+        },
+        smallButton: {
+          text: title,
+          modifiers: vmods,
+          smodifiers: ['small'],
+        },
+        smallButton_c: {
+          text: null,
+          icons: true,
+          modifiers: vmods,
+          smodifiers: ['circle','small'],
+        },
+        shadowButton: {
+          text: title,
+          modifiers: vmods,
+          smodifiers: ['shadow'],
+        },
+        shadowButton_c: {
+          text: null,
+          icons: true,
+          modifiers: vmods,
+          smodifiers: ['circle','shadow'],
         },
       },
     };
@@ -56,6 +128,7 @@ module.exports = {
     text: 'Button',
     modifiers:""
   },
+  collated: false,
   default: "default",
   variants: variants()
 };
