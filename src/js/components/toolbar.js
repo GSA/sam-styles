eb2584509441dc47af0cba8e31f08393b84ec5b2
Alejandro Colina <jose.colina@gsa.gov>
2020-07-17 13:12:06 -0400

    Added base

--- src/js/components/toolbar.js
+++ src/js/components/toolbar.js
@@ -1,16 +1,19 @@
 const samToolbar = document.querySelector(".sds-toolbar");
-if(samToolbar){
+if (samToolbar) {
   const samToolbarContent = samToolbar.querySelector(".sds-toolbar__content");
   const samToolbarSibling = samToolbar.nextElementSibling;
   const samToolbarToggleBtn = samToolbar.querySelector(
     ".sds-toolbar__toogle-btn"
   );
   const samPageContent = document.querySelector(".sds-page__content");
-  
-  samToolbarToggleBtn.addEventListener("click", event => {
+
+  samToolbarToggleBtn.addEventListener("click", (event) => {
     if (!samToolbar.classList.contains("sds-toolbar--expanded")) {
       const startExpandedAnim = () => {
-        samToolbarSibling.removeEventListener("transitionend", startExpandedAnim);
+        samToolbarSibling.removeEventListener(
+          "transitionend",
+          startExpandedAnim
+        );
         samToolbar.classList.add("sds-toolbar--expanded");
       };
       requestAnimationFrame(() => {
@@ -32,5 +35,40 @@ if(samToolbar){
       });
     }
     event.preventDefault();
+  });
+}
+
+const samMobileToolbar = document.querySelector(".sds-toolbar-mobile__select");
+if (samMobileToolbar) {
+  const samMobileToolbarToggleBtn = samMobileToolbar.querySelectorAll(
+    ".sds-toolbar-mobile__select-button"
+  );
+  console.log(samMobileToolbarToggleBtn);
+  samMobileToolbarToggleBtn.forEach(function (targetmobile) {
+    targetmobile.addEventListener(
+      "click",
+      (event) => {
+        let target = event.currentTarget.getAttribute("data-show_target");
+        document.getElementById(target).toggleAttribute("hidden");
+      },
+      true
+    );
+  });
+  
+  const samMobileRetunToolbarToggleBtn = document.querySelectorAll(
+    ".sds-toolbar-mobile__header-button"
+  );
+  
+  console.log("FAA",samMobileRetunToolbarToggleBtn);
+  samMobileRetunToolbarToggleBtn.forEach(function (targetmobile) {
+    targetmobile.addEventListener(
+      "click",
+      (event) => {
+        //let target = event.currentTarget.getAttribute("data-show_target");
+  console.log("fooo", event.currentTarget);
+        document.getElementById(event.currentTarget).toggleAttribute("hidden");
+      },
+      true
+    );
   });
 }
