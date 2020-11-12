08d63583e49a53933633e032fc58bd43105ad4a0
Anna Shchelokova <anna.shchelokova@gsa.gov>
2020-11-12 10:34:48 -0500

    collapsible cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 const sam_toolbar = require('./components/toolbar');
 const sam_accordion = require('./components/accordion');
 const sam_alert = require ('./components/alert');
+const sam_collapse= require('./components/collapsecard');
