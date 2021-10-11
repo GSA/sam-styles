1cc43416e589d171946fb74015d34c5a9a8b6865
Cameron Wolf <cameron.wolf@gsa.gov>
2021-10-11 07:56:38 -0400

    Switcher fixes that work locally

--- src/js/components/switcher.js
+++ src/js/components/switcher.js
@@ -1,6 +1,6 @@
 const uswdsStyleSheet = 'uswds.css';
 const sdsStyleSheet ='sds.css';
-const stylesheetPathRegex = /((?:\.\.\/)*\/?)(?:sds|uswds)\.css/;
+const stylesheetPathRegex = /((?:\.\.\/)*\/?)(?:sds|uswds)\.css/g;
 
 if(localStorage.getItem('styleToApply') === null){
     localStorage.setItem('styleToApply', 'sds');
@@ -19,14 +19,20 @@ function updateStyleSheet(){
     let prefix = '';
     for (let i = 0; i < links.length; i++) {
         const element = links.item(i);
+        console.log('href', element.getAttribute('href'))
         const matches = stylesheetPathRegex.exec(element.getAttribute('href'));
+        console.log('lastIndex', stylesheetPathRegex.lastIndex)
         if(stylesheetPathRegex.lastIndex !== 0){
             index = i;
             prefix = matches[1];
+            console.log('matches[1]', matches[1])
             break;
         }
     }
-    links.item(index).setAttribute('href', prefix + localStorage.getItem('styleToApply') === 'sds' ? sdsStyleSheet : uswdsStyleSheet);
+    console.log('prefix', prefix)
+    console.log('styleToApply', localStorage.getItem('styleToApply'))
+    console.log('href', prefix + (localStorage.getItem('styleToApply') === 'sds') ? sdsStyleSheet : uswdsStyleSheet);
+    links.item(index).setAttribute('href', prefix + (localStorage.getItem('styleToApply') === 'sds' ? sdsStyleSheet : uswdsStyleSheet));
     setTimeout(()=>{
         containingDiv.hidden = null;
     }, 50)
