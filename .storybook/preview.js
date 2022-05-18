4712a4c450977ede328733e8a598111fce3a12c2
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-05-18 06:58:23 -0400

    storybook main js files

---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+import "!style-loader!css-loader!sass-loader!../scss/sam-styles/index.scss";
