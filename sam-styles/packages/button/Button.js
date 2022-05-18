c946316f990d0f67aca9e46ded2f4ac48d780706
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-05-18 10:32:02 -0400

    css changes button

--- sam-styles/packages/button/Button.js
+++ sam-styles/packages/button/Button.js
@@ -1,13 +1,12 @@
-export const createButton = ({ primary = false, size = "medium", backgroundColor, label, onClick }) => {
+export const createButton = ({ primary = false, type = "primary", hover = false, label, onClick }) => {
   const btn = document.createElement("button");
   btn.type = "button";
   btn.innerText = label;
   btn.addEventListener("click", onClick);
 
-  const mode = primary ? "usa-button" : "usa-button--secondary";
-  btn.className = ["usa-button", mode].join(" ");
-
-  btn.style.backgroundColor = backgroundColor;
+  const mode = primary ? "usa-button" : mode;
+  const hoverclass = hover ? "usa-button--hover" : "";
+  btn.className = ["usa-button", mode, hoverclass].join(" ");
 
   return btn;
 };
