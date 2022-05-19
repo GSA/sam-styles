3db65edab9175a28790a24434df4a9b6da390a33
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-05-18 23:05:58 -0400

    button demo added

--- sam-styles/packages/button/Button.js
+++ sam-styles/packages/button/Button.js
@@ -1,12 +1,13 @@
-export const createButton = ({ primary = false, type = "primary", hover = false, label, onClick }) => {
+export const createButton = ({ type, size, focusType, label, onClick }) => {
   const btn = document.createElement("button");
   btn.type = "button";
   btn.innerText = label;
   btn.addEventListener("click", onClick);
 
-  const mode = primary ? "usa-button" : mode;
-  const hoverclass = hover ? "usa-button--hover" : "";
-  btn.className = ["usa-button", mode, hoverclass].join(" ");
+  const typeD = type == "shadow" || type == "white" || type == "danger" ? `sds-button--${type}` : `usa-button--${type}`;
+  const sizeD = size === "big" ? `usa-button--${size}` : size === "small" ? "sds-button--small" : "";
+  const focus = focusType && focusType !== "focus" ? `usa-button--${focusType}` : focusType ? "usa-focus" : "";
+  btn.className = ["usa-button", typeD, focus, sizeD].join(" ");
 
   return btn;
 };
--- sam-styles/packages/button/Button.stories.js
+++ sam-styles/packages/button/Button.stories.js
@@ -1,49 +1,31 @@
 import { createButton } from "./Button";
 
-// More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
   title: "Example/Button",
-  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     label: { control: "text" },
-    hover: { control: "boolean" },
     onClick: { action: "onClick" },
-    primary: { control: "boolean" },
     type: {
       control: { type: "select" },
-      options: ["secondary", "accent", "danger"],
+      options: ["", "secondary", "base", "unstyled", "accent-cool", "outline", "shadow", "white", "danger"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["big", "small"],
+    },
+    focusType: {
+      control: { type: "select" },
+      options: ["", "hover", "active", "focus", "disabled"],
     },
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/html/writing-stories/introduction#using-args
 const Template = ({ label, ...args }) => {
-  // You can either use a function to create DOM elements or use a plain html string!
-  // return `<div>${label}</div>`;
   return createButton({ label, ...args });
 };
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/html/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: "Button",
-};
+export const Default = Template.bind({});
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-  type: "secondary",
-};
-
-export const Base = Template.bind({});
-Base.args = {
-  size: "base",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
+Default.args = {
   label: "Button",
 };
