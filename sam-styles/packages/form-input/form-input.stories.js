6797d8a7b18e6f1f795e5309f33c1c55d7d95006
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2022-07-01 00:16:45 -0400

    form input changes

---
+++ sam-styles/packages/form-input/form-input.stories.js
@@ -0,0 +1,26 @@
+export default {
+  title: "FormControls/Input",
+  argTypes: {
+    label: {type: "string"},
+    classTypes: {
+      control: {type: "select"},
+      options: ["", "usa-focus", "usa-input--error", "usa-input--success"],
+    },
+  },
+};
+const Template = (args) => {
+  return ` 
+<label class="usa-label" for="input-type-text">${args.label}</label>
+<input class="usa-input ${args.classTypes}" id="input-type-text" name="input-type-text" type="text">
+`;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Text input label",
+};
+
+// export const Focus = Template.bind({});
+// Focus.args = {
+//   class: "usa-focus",
+// };
