7b614a847018e307190030bd3be936e67a13b48e
Shayan Roshan <shayan-roshan@gsa.gov>
2024-01-25 11:08:29 -0600

    remove unused code

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,7 +3,6 @@ import { create } from '@storybook/theming';
 
 addons.setConfig({
   theme: create({
-    base: 'light', // This sets the theme to light mode
-    // You can also customize other theme properties if needed
+    base: 'light',
   }),
 });
