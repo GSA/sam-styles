46ca5bb608d29b3b2eeed0cc46d9d0d4abc234bc
Shayan Roshan <shayan-roshan@gsa.gov>
2024-02-20 08:22:36 -0600

    add uswds js

--- sam-styles/packages/components/accordion/accordion.stories.js
+++ sam-styles/packages/components/accordion/accordion.stories.js
@@ -17,9 +17,9 @@ export default {
             Accordion Button
         </button>
     </h2>
-    <!--div id="b-a1" class="usa-accordion__content">
+    <div id="b-a1" class="usa-accordion__content" hidden>
         <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
-    </div-->
+    </div>
 
     <h2 class="usa-accordion__heading">
         <button class="usa-accordion__button " aria-expanded="true" aria-controls="b-a2">
@@ -35,27 +35,27 @@ export default {
             Accordion Button
         </button>
     </h2>
-    <!--div id="b-a3" class="usa-accordion__content">
+    <div id="b-a3" class="usa-accordion__content" hidden>
         <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
-    </div-->
+    </div>
 
     <h2 class="usa-accordion__heading">
         <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a4">
             Accordion Button
         </button>
     </h2>
-    <!--div id="b-a4" class="usa-accordion__content">
+    <div id="b-a4" class="usa-accordion__content" hidden>
         <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
-    </div-->
+    </div>
 
     <h2 class="usa-accordion__heading">
         <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a5">
             Accordion Button
         </button>
     </h2>
-    <!--div id="b-a5" class="usa-accordion__content">
+    <div id="b-a5" class="usa-accordion__content" hidden>
         <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
-    </div-->
+    </div>
 
 </div>`;
   };
