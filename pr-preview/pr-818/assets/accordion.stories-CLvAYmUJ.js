4e878c434689da6cc8f83fa8261caa943a9a251b
fpigeonjr <fpigeonjr@users.noreply.github.com>
2026-08-25 20:01:05 +0000

    Deploy preview for PR 818 🛫

---
+++ pr-preview/pr-818/assets/accordion.stories-CLvAYmUJ.js
@@ -0,0 +1,194 @@
+import{r as e}from"./preload-helper-Dl2UHooj.js";var t,n=e((()=>{t=`<div
+  class="usa-accordion usa-accordion--bordered"
+  aria-multiselectable="true"
+  data-allow-multiple
+>
+  <h2 class="usa-accordion__heading">
+    <button
+      class="usa-accordion__button border-top-1px border-base-light"
+      aria-expanded="true"
+      aria-controls="m-a1"
+    >
+      Accordion Button
+    </button>
+  </h2>
+  <div id="m-a1" class="usa-accordion__content">
+    <p>
+      Congress shall make no law respecting an establishment of religion, or
+      prohibiting the free exercise thereof; or abridging the freedom of speech,
+      or of the press; or the right of the people peaceably to assemble, and to
+      petition the Government for a redress of grievances.
+    </p>
+  </div>
+
+  <h2 class="usa-accordion__heading">
+    <button
+      class="usa-accordion__button"
+      aria-expanded="false"
+      aria-controls="m-a2"
+    >
+      Accordion Button
+    </button>
+  </h2>
+  <div id="m-a2" class="usa-accordion__content" hidden>
+    <p>
+      Congress shall make no law respecting an establishment of religion, or
+      prohibiting the free exercise thereof; or abridging the freedom of speech,
+      or of the press; or the right of the people peaceably to assemble, and to
+      petition the Government for a redress of grievances.
+    </p>
+  </div>
+
+  <h2 class="usa-accordion__heading">
+    <button
+      class="usa-accordion__button"
+      aria-expanded="false"
+      aria-controls="m-a3"
+    >
+      Accordion Button
+    </button>
+  </h2>
+  <div id="m-a3" class="usa-accordion__content" hidden>
+    <p>
+      Congress shall make no law respecting an establishment of religion, or
+      prohibiting the free exercise thereof; or abridging the freedom of speech,
+      or of the press; or the right of the people peaceably to assemble, and to
+      petition the Government for a redress of grievances.
+    </p>
+  </div>
+
+  <h2 class="usa-accordion__heading">
+    <button
+      class="usa-accordion__button"
+      aria-expanded="true"
+      aria-controls="m-a4"
+    >
+      Accordion Button
+    </button>
+  </h2>
+  <div id="m-a4" class="usa-accordion__content">
+    <p>
+      Congress shall make no law respecting an establishment of religion, or
+      prohibiting the free exercise thereof; or abridging the freedom of speech,
+      or of the press; or the right of the people peaceably to assemble, and to
+      petition the Government for a redress of grievances.
+    </p>
+  </div>
+
+  <h2 class="usa-accordion__heading">
+    <button
+      class="usa-accordion__button"
+      aria-expanded="false"
+      aria-controls="m-a5"
+    >
+      Accordion Button
+    </button>
+  </h2>
+  <div id="m-a5" class="usa-accordion__content" hidden>
+    <p>
+      Congress shall make no law respecting an establishment of religion, or
+      prohibiting the free exercise thereof; or abridging the freedom of speech,
+      or of the press; or the right of the people peaceably to assemble, and to
+      petition the Government for a redress of grievances.
+    </p>
+  </div>
+</div>
+`})),r,i,a,o,s;e((()=>{n(),r={title:`Components/Accordion`,argTypes:{class:{type:`string`},expanded:{type:`boolean`}}},i=e=>`<div class="usa-accordion ${e.class}">
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button  border-top-1px border-base-light " aria-expanded="false" aria-controls="b-a1">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a1" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="true" aria-controls="b-a2">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a2" class="usa-accordion__content">
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a3">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a3" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a4">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a4" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a5">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a5" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+</div>`,a=()=>t,o=i.bind({}),o.args={class:`usa-accordion--bordered`},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`() => {
+  return MultiSelectableAccordion;
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => {
+  return \`<div class="usa-accordion \${args.class}">
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button  border-top-1px border-base-light " aria-expanded="false" aria-controls="b-a1">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a1" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="true" aria-controls="b-a2">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a2" class="usa-accordion__content">
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a3">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a3" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a4">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a4" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+    <h2 class="usa-accordion__heading">
+        <button class="usa-accordion__button " aria-expanded="false" aria-controls="b-a5">
+            Accordion Button
+        </button>
+    </h2>
+    <div id="b-a5" class="usa-accordion__content" hidden>
+        <p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.</p>
+    </div>
+
+</div>\`;
+}`,...o.parameters?.docs?.source}}},s=[`Multiselectable`,`Bordered`]}))();export{o as Bordered,a as Multiselectable,s as __namedExportsOrder,r as default};
---
+++ pr-preview/pr-818/assets/sdstile.stories-BlOmDtsH.js
@@ -0,0 +1,109 @@
+import{r as e}from"./preload-helper-Dl2UHooj.js";var t,n,r,i;e((()=>{t={title:`Components/Dashboard/Tile`},n=()=>`<div class="grid-row flex-align-center">
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+  `,r=n.bind({}),r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
+  return \`<div class="grid-row flex-align-center">
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="tablet:grid-col">
+        <div class="sds-tile">
+            <span class="sds-tile__icon fa-stack fa-2x">
+                <!--i class="sds text-ink fa-leaf fa-stack-2x text-accent-cool-light"></i-->
+                <!--i class="sds text-ink fa-pen-paper fa-stack-1x text-base fa-2x"></i-->
+            </span>
+            <div class="sds-tile__body">
+                <header>SDS Tile</header>
+                <ul class="sds-list">
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+  \`;
+}`,...r.parameters?.docs?.source}}},i=[`SDStile`]}))();export{r as SDStile,i as __namedExportsOrder,t as default};
