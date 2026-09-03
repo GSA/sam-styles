d340050f3d8e3584b7f38fd18cf0e8f9387a8de4
fpigeonjr <fpigeonjr@users.noreply.github.com>
2026-09-03 19:56:55 +0000

    Remove preview for PR 820 🛬

--- pr-preview/pr-820/assets/tileoutline.stories-BbrAuX2W.js
+++
@@ -1,5 +0,0 @@
-import{t as e}from"./rolldown-runtime-Dh6celcD.js";var t,n,r,i;function a(){return(a=e((()=>{t={title:`Components/Dashboard/Tile`},n=()=>`
-  `,r=n.bind({}),r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return \`
-  \`;
-}`,...r.parameters?.docs?.source}}},i=[`SDStileOutline`]})))()}a();export{r as SDStileOutline,i as __namedExportsOrder,t as default};
