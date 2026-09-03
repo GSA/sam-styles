a8d3dbdfe48a342d9864b5c37a05551329eab8d7
fpigeonjr <frank.pigeonjr@gmail.com>
2026-09-03 19:57:31 +0000

    Deploying to gh-pages from @ GSA/sam-styles@e559f2d0ce922b107f380c3770134280fe0526ff 🚀

---
+++ assets/default.stories-C2cuKgUM.js
@@ -0,0 +1,81 @@
+import{t as e}from"./rolldown-runtime-Dh6celcD.js";var t,n,r,i;function a(){return(a=e((()=>{t={title:`Components/Dashboard/Statistics`},n=()=>`<h3 class="margin-0">SDS Statistic</h3>
+    <div class="sds-statistics">
+        <button class="green statistic">
+            <div class="value">
+                <span class="circular">150K+</span>
+            </div>
+            <div class="label">Active</div>
+        </button>
+        <button class="blue statistic">
+            <div class="value">
+                <span class="circular">999K+</span>
+            </div>
+            <div class="label">Draft</div>
+        </button>
+        <button class="red statistic">
+            <div class="value">
+                <span class="circular">112</span>
+            </div>
+            <div class="label">Rejected</div>
+        </button>
+        <button class="orange statistic">
+            <div class="value">
+                <span class="circular">209</span>
+            </div>
+            <div class="label">Overdue</div>
+        </button>
+        <button class="grey statistic">
+            <div class="value">
+                <span class="circular">5</span>
+            </div>
+            <div class="label">Cancelled</div>
+        </button>
+        <button class="yellow statistic">
+            <div class="value">
+                <span class="circular">12</span>
+            </div>
+            <div class="label">Pending</div>
+        </button>
+    </div>
+  `,r=n.bind({}),r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
+  return \`<h3 class="margin-0">SDS Statistic</h3>
+    <div class="sds-statistics">
+        <button class="green statistic">
+            <div class="value">
+                <span class="circular">150K+</span>
+            </div>
+            <div class="label">Active</div>
+        </button>
+        <button class="blue statistic">
+            <div class="value">
+                <span class="circular">999K+</span>
+            </div>
+            <div class="label">Draft</div>
+        </button>
+        <button class="red statistic">
+            <div class="value">
+                <span class="circular">112</span>
+            </div>
+            <div class="label">Rejected</div>
+        </button>
+        <button class="orange statistic">
+            <div class="value">
+                <span class="circular">209</span>
+            </div>
+            <div class="label">Overdue</div>
+        </button>
+        <button class="grey statistic">
+            <div class="value">
+                <span class="circular">5</span>
+            </div>
+            <div class="label">Cancelled</div>
+        </button>
+        <button class="yellow statistic">
+            <div class="value">
+                <span class="circular">12</span>
+            </div>
+            <div class="label">Pending</div>
+        </button>
+    </div>
+  \`;
+}`,...r.parameters?.docs?.source}}},i=[`Default`]})))()}a();export{r as Default,i as __namedExportsOrder,t as default};
--- assets/tileoutline.stories-BbClcHv5.js
+++
@@ -1,5 +0,0 @@
-import{r as e}from"./preload-helper-Dl2UHooj.js";var t,n,r,i;e((()=>{t={title:`Components/Dashboard/Tile`},n=()=>`
-  `,r=n.bind({}),r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return \`
-  \`;
-}`,...r.parameters?.docs?.source}}},i=[`SDStileOutline`]}))();export{r as SDStileOutline,i as __namedExportsOrder,t as default};
