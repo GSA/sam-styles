9086dfed317d34c71ecef1dc865d2f0632e72fac
fpigeonjr <fpigeonjr@users.noreply.github.com>
2026-09-03 15:33:22 +0000

    Deploy preview for PR 820 🛫

---
+++ pr-preview/pr-820/assets/name-form.stories-Dc5vBYsY.js
@@ -0,0 +1,37 @@
+import{t as e}from"./rolldown-runtime-Dh6celcD.js";var t,n,r,i;function a(){return(a=e((()=>{t={title:`Form Elements/NameForm`,argTypes:{label:{type:`string`},classTypes:{control:{type:`select`},options:[``,`usa-focus`,`usa-input--error`,`usa-input--success`]}}},n=e=>` 
+    <form class="usa-form">
+    <fieldset class="usa-fieldset">
+        <legend class="usa-legend">Name</legend>
+        <label class="usa-label" for="title">Title <span class="usa-hint">(optional)</span></label>
+        <input class="usa-input usa-input--small" id="title" name="title" type="text">
+
+        <label class="usa-label" for="first-name">First name</label>
+        <input class="usa-input" id="first-name" name="first-name" type="text" required aria-required="true">
+
+        <label class="usa-label" for="middle-name">Middle name <span class="usa-hint">(optional)</span></label>
+        <input class="usa-input" id="middle-name" name="middle-name" type="text">
+
+        <label class="usa-label" for="last-name">Last name</label>
+        <input class="usa-input" id="last-name" name="last-name" type="text" required aria-required="true">
+    </fieldset>
+    </form>
+    `,r=n.bind({}),r.args={label:`Text input label`},r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`args => {
+  return \` 
+    <form class="usa-form">
+    <fieldset class="usa-fieldset">
+        <legend class="usa-legend">Name</legend>
+        <label class="usa-label" for="title">Title <span class="usa-hint">(optional)</span></label>
+        <input class="usa-input usa-input--small" id="title" name="title" type="text">
+
+        <label class="usa-label" for="first-name">First name</label>
+        <input class="usa-input" id="first-name" name="first-name" type="text" required aria-required="true">
+
+        <label class="usa-label" for="middle-name">Middle name <span class="usa-hint">(optional)</span></label>
+        <input class="usa-input" id="middle-name" name="middle-name" type="text">
+
+        <label class="usa-label" for="last-name">Last name</label>
+        <input class="usa-input" id="last-name" name="last-name" type="text" required aria-required="true">
+    </fieldset>
+    </form>
+    \`;
+}`,...r.parameters?.docs?.source}}},i=[`NameForm`]})))()}a();export{r as NameForm,i as __namedExportsOrder,t as default};
