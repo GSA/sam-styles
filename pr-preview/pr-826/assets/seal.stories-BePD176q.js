2087a0f16c35a6b1fe20c08780a165dbc3030162
fpigeonjr <fpigeonjr@users.noreply.github.com>
2026-09-10 16:31:54 +0000

    Deploy preview for PR 826 🛫

---
+++ pr-preview/pr-826/assets/seal.stories-BePD176q.js
@@ -0,0 +1,10 @@
+import{t as e}from"./rolldown-runtime-Dh6celcD.js";var t;function n(){return(n=e((()=>{t=`<div class="sds-seal">
+  <i class="sds text-ink bi-flag-fill size-2x margin-y-1 text-secondary"></i>
+  <div class="sds-seal__content">
+    Official U.S. Government Website
+    <span>100% Free</span>
+  </div>
+</div>
+`})))()}var r,i,a;function o(){return(o=e((()=>{n(),r={title:`Structure/Seal`},i=()=>t,i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`() => {
+  return SealStructure;
+}`,...i.parameters?.docs?.source}}},a=[`Seal`]})))()}o();export{i as Seal,a as __namedExportsOrder,r as default};
